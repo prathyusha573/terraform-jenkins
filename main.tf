c977969e001d52ab971409467a3236d6b2c82226
prathyusha <prathyushareddyd9@gmail.com>
2025-02-11 22:01:09 -0500

    modified main

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-/*terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.18.0"
-    }
-  }
-}
-provider "azurerm" {
-  features {}
-
-  subscription_id = var.subscription_id
-}
-*/
-
 terraform {
   required_providers {
     azurerm = {
